cffe48bd5f5a6b8c3ab19b90fd864585dc7699e4
krishna kumar <krishnau2@gmail.com>
2016-10-09 02:52:33 +0530

    Basic timer functionality is done.
    - Once the Pomodoro is completed the 5 min break counter will start
    - Once 4 Pomodoros completed then 15 min break counter will start
    
    - Added basic desktop notifications feature.
    - Comodoro log is added for Notification icon
    
    TODO:
    - Need to show proper notification message based on Pomodoro, Short break, Long break

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import CountDownTimer from './timer';
-// import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 class App extends Component {
@@ -14,8 +14,8 @@ class App extends Component {
     this.longBreak = 15*60;
     this.state = {completedPomodors: 0,
                   currentTimer: 'pomodoro',
-                  duration: this.pomodoroDuration,
-                  time: this.pomodoroTime
+                  duration: 1*60,
+                  time: '01:00'
                 };
     this.handleTimerCompleted = this.handleTimerCompleted.bind(this);
   }
@@ -23,11 +23,18 @@ class App extends Component {
   handleTimerCompleted(){
     console.log('handleTimerCompleted is called.....');
     var duration, time, currentTimer, completedPomodors;
+    this.desktopNotification();
     if(this.state.currentTimer === 'pomodoro'){
       completedPomodors = this.state.completedPomodors + 1;
-      duration = this.shortBreak;
-      time = this.shortBreakTime;
-      currentTimer = 'break'
+      if(completedPomodors % 4 === 0){
+        duration = this.longBreak;
+        time = this.longBreakTime;
+        currentTimer = 'break'
+      }else{
+        duration = this.shortBreak;
+        time = this.shortBreakTime;
+        currentTimer = 'break'
+      }
     }else{
       completedPomodors = this.state.completedPomodors;
       duration = this.pomodoroDuration;
@@ -40,6 +47,32 @@ class App extends Component {
                     duration: duration,
                     time: time
                   });
+  }
+
+  desktopNotification(){
+    // // request permission on page load
+    // document.addEventListener('DOMContentLoaded', function () {
+    //   if (Notification.permission !== "granted")
+    //     Notification.requestPermission();
+    // });
+    if (!Notification) {
+      alert('Desktop notifications not available in your browser.');
+      return;
+    }
+
+    if (Notification.permission !== "granted")
+      Notification.requestPermission();
+    else {
+      var notification = new Notification('Notification title', {
+        icon: logo,
+        body: "Hey there! You've been notified!",
+      });
+      // notification.onclick = function () {
+      //   window.open("http://stackoverflow.com/a/13328397/1269037");
+      // };
+
+    }
+
   }
 
   render() {
