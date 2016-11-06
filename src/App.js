028017617b6510a4f8dd473ac00900ab6843ed2b
krishna kumar <krishnau2@gmail.com>
2016-11-07 01:38:48 +0530

    For testing logo in production.

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
 
     this.state = {duration: this.pomodoroDuration,
                   status: 'initial',
-                  completedBlock: 0,
+                  completedBlock: 1,
                   timerLeft: this.baseLeft,
                   timerStartingPosition: 0,
                   progressbarLeft: 0,
